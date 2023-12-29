2fca4b5b37e62bdcb9c5c403f14ada37c2991051
Abolfazl Soltani <absoltani02@gmail.com>
2023-12-29 13:45:08 +0330

    add an empty struct implementing Broker interface

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -1,1 +1,28 @@
 package broker
+
+type SimpleBroker struct {
+}
+
+func NewBroker() Broker {
+	return &SimpleBroker{}
+}
+
+func (b *SimpleBroker) AddQueue(name string, isMaster bool) error {
+	return nil
+}
+
+func (b *SimpleBroker) SetQueueMaster(name string, masterStatus bool) error {
+	return nil
+}
+
+func (b *SimpleBroker) QueuePush(name string, value []byte) error {
+	return nil
+}
+
+func (b *SimpleBroker) QueuePop(name string) ([]byte, error) {
+	return nil, nil
+}
+
+func (b *SimpleBroker) Pop() (string, []byte, error) {
+	return "", nil, nil
+}
